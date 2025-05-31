81bcc1483fafc2515198a3fb378bae66c17246f2
Djillali Tareb <djitrb100@gmail.com>
2025-05-31 10:00:11 +0100

    commentaires

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import './i18n'; // Import i18n configuration before rendering the app
+import './i18n'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
